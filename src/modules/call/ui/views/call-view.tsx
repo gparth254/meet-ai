9001730ea3b258f60d3a0bce1acd28c9d2b8bc6b
gparth254 <gparth254@gmail.com>
2025-06-26 00:34:50 +0530

    22 video call

---
+++ src/modules/call/ui/views/call-view.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+
+import {CallProvider} from "../components/call-provider";
+import { useTRPC } from "@/trpc/client";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import {ErrorState} from "@/components/error-state";
+interface Props {
+  meetingId: string;
+}
+
+export const CallView = ({ meetingId }: Props) => {
+  const trpc = useTRPC();
+  const { data } = useSuspenseQuery(
+    trpc.meetings.getOne.queryOptions({ id: meetingId })
+  );
+  if(data.status === "completed"){
+    return (
+         <div className ="flex h-screen items-center justify-center">
+            <ErrorState 
+             title="Meeting Completed"
+             description="Meeting was completed successfully"
+           
+            
+            
+            
+            
+            />
+         </div>
+
+    )
+
+}
+  return  <CallProvider meetingId={meetingId} meetingName={data.name} />
+};
